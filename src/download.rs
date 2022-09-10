a478526130fb9680507fa31f4e5c1127ad3ae076
Tristan Fox <tristanfox009@outlook.com>
2022-09-11 09:28:32 +1000

    Fix TUI and speed improve downloads

--- src/download.rs
+++ src/download.rs
@@ -1,80 +1,159 @@
 use std::{
-    fs,
-    io::{self, copy, Read},
+    cmp::min,
+    fs::File,
+    io::Write,
     path::{Path, PathBuf},
 };
 
-use exitfailure::ExitFailure;
+use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::Url;
-use reqwest::{header, Client};
+use reqwest::Client;
 
-struct DownloadProgress<R: std::io::Read> {
-    inner: R,
-    progress_bar: ProgressBar,
-}
+pub async fn download_file<P: AsRef<Path>>(
+    client: &Client,
+    url: &str,
+    path: P,
+) -> Result<PathBuf, failure::Error> {
+    // Reqwest setup
+    let res = client
+        .get(url)
+        .send()
+        .await
+        .or(Err(failure::err_msg(format!(
+            "Failed to GET from '{}'",
+            url
+        ))))?;
+    let total_size = res.content_length().ok_or(failure::err_msg(format!(
+        "Failed to get content length from '{}'",
+        &url
+    )))?;
 
-impl<R: std::io::Read> Read for DownloadProgress<R> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        self.inner.read(buf).map(|n| {
-            self.progress_bar.inc(n as u64);
-            n
-        })
-    }
-}
-
-pub async fn download(url: &str, folder: PathBuf) -> Result<PathBuf, ExitFailure> {
-    let url = Url::parse(url)?;
-    let client = Client::new();
-
-    let total_size = {
-        let resp = client.head(url.as_str()).send().await?;
-        if resp.status().is_success() {
-            resp.headers()
-                .get(header::CONTENT_LENGTH)
-                .and_then(|ct_len| ct_len.to_str().ok())
-                .and_then(|ct_len| ct_len.parse().ok())
-                .unwrap_or(0)
-        } else {
-            return Err(failure::err_msg(format!(
-                "Couldn't download URL: {}. Error: {:?}",
-                url,
-                resp.status(),
-            ))
-            .into());
-        }
-    };
-
-    let mut request = client.get(url.as_str());
+    // Indicatif setup
     let pb = ProgressBar::new(total_size);
     pb.set_style(ProgressStyle::default_bar()
-                 .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")?
-                 .progress_chars("#>-"));
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})").unwrap()
+        .progress_chars("#>-"));
+    pb.set_message(format!("Downloading {}", url));
 
-    let file = folder.join(Path::new(
-        url.path_segments()
-            .and_then(|segments| segments.last())
-            .unwrap_or("tmp.bin"),
-    ));
+    // download chunks
+    let mut file = File::create(&path).or(Err(failure::err_msg(format!(
+        "Failed to create file '{}'",
+        path.as_ref().to_str().unwrap()
+    ))))?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
 
-    if file.exists() {
-        let size = file.metadata()?.len() - 1;
-        request = request.header(header::RANGE, format!("bytes={}-", size));
-        pb.inc(size);
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(failure::err_msg(format!(
+            "Error while downloading file"
+        ))))?;
+        file.write_all(&chunk).or(Err(failure::err_msg(format!(
+            "Error while writing to file"
+        ))))?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
     }
 
-    let bytes = request.send().await?.bytes().await?;
-    let mut source = DownloadProgress {
-        progress_bar: pb,
-        inner: bytes.as_ref(),
-    };
+    pb.finish_with_message(format!(
+        "Downloaded {} to {}",
+        url,
+        path.as_ref().to_str().unwrap()
+    ));
 
-    let mut dest = fs::OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(&file)?;
-
-    let _ = copy(&mut source, &mut dest)?;
-
-    Ok(file)
+    Ok(path.as_ref().to_path_buf())
 }
+
+// struct DownloadProgress<'a, R: std::io::Read> {
+//     inner: R,
+//     progress_bar: &'a ProgressBar,
+// }
+
+// impl<'a, R: std::io::Read> Read for DownloadProgress<'a, R> {
+//     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+//         self.inner.read(buf).map(|n| {
+//             self.progress_bar.inc(n as u64);
+//             n
+//         })
+//     }
+// }
+
+// pub async fn download(
+//     url: &str,
+//     folder: PathBuf,
+//     pb: &ProgressBar,
+// ) -> Result<PathBuf, ExitFailure> {
+//     let url = Url::parse(url)?;
+//     let client = Client::new();
+
+//     let total_size = {
+//         let resp = client.head(url.as_str()).send().await?;
+//         if resp.status().is_success() {
+//             resp.headers()
+//                 .get(header::CONTENT_LENGTH)
+//                 .and_then(|ct_len| ct_len.to_str().ok())
+//                 .and_then(|ct_len| ct_len.parse().ok())
+//                 .unwrap_or(0)
+//         } else {
+//             return Err(failure::err_msg(format!(
+//                 "Couldn't download URL: {}. Error: {:?}",
+//                 url,
+//                 resp.status(),
+//             ))
+//             .into());
+//         }
+//     };
+
+//     let mut request = client.get(url.as_str());
+//     pb.set_length(total_size);
+//     pb.set_style(
+//         ProgressStyle::default_bar()
+//             .template(
+//                 "{spinner} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})",
+//             )?
+//             .progress_chars("#>-"),
+//     );
+
+//     let file = folder.join(Path::new(
+//         url.path_segments()
+//             .and_then(|segments| segments.last())
+//             .unwrap_or("tmp.bin"),
+//     ));
+
+//     if file.exists() {
+//         let size = file.metadata()?.len() - 1;
+//         request = request.header(header::RANGE, format!("bytes={}-", size));
+//         pb.inc(size);
+//     }
+
+//     // let bytes = request.send().await?.bytes().await?;
+//     // let mut source = DownloadProgress {
+//     //     progress_bar: pb,
+//     //     inner: bytes.as_ref(),
+//     // };
+
+//     let mut res = reqwest::get(url).await?;
+
+//     let mut index: u64 = 0;
+
+//     while let Some(chunk) = res.chunk().await? {
+//         index += chunk.len() as u64;
+//         pb.set_position(index);
+//     }
+
+//     // let mut source = DownloadProgress {
+//     //     progress_bar: pb,
+//     //     inner: res.chunk().await?.unwrap(),
+//     // }
+
+//     pb.finish();
+
+//     let mut dest = fs::OpenOptions::new()
+//         .create(true)
+//         .append(true)
+//         .open(&file)?;
+
+//     // let _ = copy(&mut source, &mut dest)?;
+
+//     Ok(file)
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Args, Parser, Subcommand};
 use indicatif::{ProgressBar, ProgressStyle};
-use std::{fs, process};
+use reqwest::Client;
+use std::{fs, process, sync::Arc, thread, time::Duration};
+use tokio::sync::Mutex;
 
 use voran::{
     package::PackageType,
@@ -18,31 +20,59 @@ async fn main() {
             let mut repository = packages::get_packages().git();
 
             let remotes = repository.remotes();
-            let pb = ProgressBar::new(remotes.len().saturating_sub(1) as u64);
+            let remotes_len = remotes.len();
+
+            let index: Arc<Mutex<u64>> = Arc::new(Mutex::new(0));
+
+            let pb = ProgressBar::new(remotes_len.saturating_sub(1) as u64);
             pb.set_style(
                 ProgressStyle::default_bar()
-                    .template("[{bar:40.cyan/blue}]")
+                    .template("{spinner} [{bar:40.cyan/blue}] Setting up remotes...")
                     .unwrap()
                     .progress_chars("#>-"),
             );
-            for (index, remote) in remotes.iter().enumerate() {
-                repository.remove_remote(&remote);
-                pb.set_position(index as u64);
-            }
+
+            tokio_scoped::scope(|s| {
+                s.spawn(async {
+                    while index.lock().await.saturating_add(0) < pb.length().unwrap() {
+                        pb.set_position(index.lock().await.saturating_add(0));
+                        thread::sleep(Duration::from_millis(100));
+                    }
+                    pb.finish();
+                });
+                s.spawn(async {
+                    for (i, remote) in remotes.iter().enumerate() {
+                        repository.remove_remote(&remote);
+                        *index.lock().await = i as u64;
+                    }
+                });
+            });
 
             let pb = ProgressBar::new(config.git_repo_urls.len().saturating_sub(1) as u64);
             pb.set_style(
                 ProgressStyle::default_bar()
-                    .template("[{bar:40.cyan/blue}]")
+                    .template("{spinner} [{bar:40.cyan/blue}] Pulling remotes...")
                     .unwrap()
                     .progress_chars("#>-"),
             );
-            for (index, (name, url)) in config.git_repo_urls.iter().enumerate() {
-                repository.add_remote(&name, &url);
+            let index: Arc<Mutex<u64>> = Arc::new(Mutex::new(0));
+            tokio_scoped::scope(|s| {
+                s.spawn(async {
+                    while index.lock().await.saturating_add(0) < pb.length().unwrap() {
+                        pb.set_position(index.lock().await.saturating_add(0));
+                        thread::sleep(Duration::from_millis(100));
+                    }
+                    pb.finish();
+                });
+                s.spawn(async {
+                    for (i, (name, url)) in config.git_repo_urls.iter().enumerate() {
+                        repository.add_remote(&name, &url);
 
-                repository.pull(&name);
-                pb.set_position(index as u64);
-            }
+                        repository.pull(&name);
+                        *index.lock().await = i as u64;
+                    }
+                });
+            });
             println!("Update successful");
         }
         Command::Install(args) => {
@@ -62,9 +92,13 @@ async fn main() {
             if !cache_dir.exists() {
                 fs::create_dir_all(cache_dir).unwrap();
             }
-            let out_file = voran::download(&package.install.url, cache_dir.to_path_buf())
-                .await
-                .expect("Failed to download file");
+            let out_file = voran::download_file(
+                &Client::new(),
+                &package.install.url,
+                cache_dir.join(format!("{}.jellyfish", package.name)),
+            )
+            .await
+            .expect("Failed to download file");
             println!("Download complete!, Installing...");
 
             match package.install.type_ {
